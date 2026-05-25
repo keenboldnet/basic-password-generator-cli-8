cbb31e3698fa919c85b6796cf0c772a0fffa1c6f
Sage Scott <agaceritakmarunidu06@gmail.com>
2026-05-25 05:00:43 -0400

    Add batch generation and custom character sets

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import random
 import string
 
-def create_password(size=12):
-    characters = string.ascii_letters + string.digits + string.punctuation
+def create_password(size=12, use_upper=True, use_digits=True, use_special=True):
+    characters = string.ascii_lowercase
+    if use_upper:
+        characters += string.ascii_uppercase
+    if use_digits:
+        characters += string.digits
+    if use_special:
+        characters += string.punctuation
     password = ''.join(random.choice(characters) for _ in range(size))
     return password
 
+def generate_batch(count=5, size=12):
+    passwords = []
+    for _ in range(count):
+        passwords.append(create_password(size))
+    return passwords
+
 if __name__ == "__main__":
-    print(create_password())
+    for i, pwd in enumerate(generate_batch(), 1):
+        print(f"{i}. {pwd}")
